d7f9ae06108ce13b86ba8246c46ff92266442f30
Farzan khan <farzaan341@gmail.com>
2026-08-13 00:48:01 +0500

    Add files via upload

---
+++ j.s11/app.js
@@ -0,0 +1,41 @@
+
+for (let i = 0; i <= 10; i++) {
+   console.log(i);
+   
+    
+}
+
+
+for (let ind = 1; ind<=20; ind++) {
+    if (ind%2===0) {
+        console.log(ind);
+        
+    }
+}
+
+
+
+
+
+
+let fruits = ["Apple", "Mango", "Banana", "Orange", "Grapes"];
+
+for (let index = 0; index < fruits.length; index++) {
+     console.log(fruits[index]);
+     
+    
+}
+
+
+
+
+
+let numbers = [10, 25, 30, 45, 50, 65, 70];
+
+for (let i = 0; i < numbers.length; i++) {
+
+    if (numbers[i] > 40) {
+        console.log(numbers[i]);
+    }
+
+}
